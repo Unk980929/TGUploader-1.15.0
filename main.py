c58768c2f03d3282b0ceb3d8ff297616e7f79cd4
Unk980929 <102277711+Unk980929@users.noreply.github.com>
2022-05-01 21:51:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -622,7 +622,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 7386053
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5346211856:AAEbFPpQwF4g1DcrM2T5WqA57Moz_rVrh_E'
+            #    bot_token = '5300528243:AAEDykyNQ6e-WJhURg_TAyC8J69r9axSwcA'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -638,7 +638,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5346211856:AAEbFPpQwF4g1DcrM2T5WqA57Moz_rVrh_E'
+    bot_token = '5300528243:AAEDykyNQ6e-WJhURg_TAyC8J69r9axSwcA'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
